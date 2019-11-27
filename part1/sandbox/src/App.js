383383eb8a1230fdd92fe88f9cef00771a19c474
Carl B <petter@axesnet.com>
2019-11-27 15:50:48 +0100

    updating repo

---
+++ part1/sandbox/src/App.js
@@ -0,0 +1,73 @@
+import React,{ useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+
+const App = (props) => {
+  // const { notes } = props
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  // if showAll , notes, else use the filter
+
+  const hook = () =>{
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+    }
+  useEffect(hook,[])
+  console.log('render',notes.length,'notes');
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  const rows = () => notesToShow.map(note =>
+         <Note key={note.id} note={note}/>
+        )
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+    console.log('button clicked',event.target);
+  }
+
+  console.log("notes:",notes);
+  const handleNoteChange = (event) => {
+  console.log(event.target.value)
+  setNewNote(event.target.value)
+  }
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+       <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? 'important' : 'all'}
+       </button>
+      </div>
+      <ul>
+        {rows()}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/sandbox/src/index2.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom';
+// part 1 sandbox
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Hello = ({ name, age }) => {
+  // this gets both values from props
+    const bornYear = () => new Date().getFullYear() - age
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p> So you are probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = (props) => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  const hello = (who) => () => {
+      console.log('hello', who)
+    }
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left'/>
+        <Button onClick={handleRightClick} text='right'/>
+        {right}
+        <History allClicks={allClicks}/>
+        <button onClick={hello('world')}>button</button>
+        <button onClick={hello('react')}>button</button>
+        <button onClick={hello('function')}>button</button>
+        {value}
+        <button onClick={setToValue(1000)}>thousand</button>
+        <button onClick={setToValue(0)}>reset</button>
+        <button onClick={setToValue(value + 1)}>increment</button>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
