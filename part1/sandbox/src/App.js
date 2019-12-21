1d1a2577c7d8bca07f9a4d0816e69abdf67bbe49
Carl B <petter@axesnet.com>
2019-12-21 12:47:29 +0100

    before further removing response data from apps.je and into service/notes.js

--- part1/sandbox/src/App.js
+++ part1/sandbox/src/App.js
@@ -1,6 +1,7 @@
 import React,{ useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 
 const App = (props) => {
@@ -10,15 +11,29 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true)
   // if showAll , notes, else use the filter
 
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    // find the note we want to modify and assign it to the note variable
+    const note = notes.find(n => n.id === id)
+    // create a new object that is an exact copy but with the important property
+    const changedNote = { ...note, important: !note.important }
+
+    console.log('effect');
+    noteService
+      .update(id, changedNote)
+      .then(response =>{
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
   const hook = () =>{
     console.log('effect');
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
+    noteService
+      .getAll()
+      .then(response =>{
         setNotes(response.data)
-      })
-    }
+    })
+  }
   useEffect(hook,[])
   console.log('render',notes.length,'notes');
 
@@ -27,7 +42,11 @@ const App = (props) => {
     : notes.filter(note => note.important === true)
 
   const rows = () => notesToShow.map(note =>
-         <Note key={note.id} note={note}/>
+         <Note
+         key={note.id}
+         note={note}
+         toggleImportance={() => toggleImportanceOf(note.id)}
+         />
         )
 
   const addNote = (event) => {
@@ -41,10 +60,12 @@ const App = (props) => {
     // setNotes(notes.concat(noteObject))
     // setNewNote('')
     // console.log('button clicked',event.target);
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    .then(response => {
-      console.log(response)
+    noteService
+      .create(noteObject)
+      .then(response => {
+        console.log(response)
+        setNotes(notes.concat(response.data))
+        setNewNote('')
     })
   }
 
