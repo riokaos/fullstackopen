9741b65458e70c0a5d13d7bd04ae6c78dc56e0d9
Carl B <petter@axesnet.com>
2019-12-27 16:25:38 +0100

    preparation for styles

--- part1/sandbox/src/services/notes.js
+++ part1/sandbox/src/services/notes.js
@@ -5,14 +5,7 @@ const baseUrl = 'http://localhost:3001/notes'
 //we assign the promise to the request variable and call its then method:
 const getAll = () => {
   const request = axios.get(baseUrl)
-  const nonExisting = {
-    id: 10000,
-    content: 'This note is not saved to server',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  }
-  return request.then(response => response.data.concat(nonExisting))
-  // return request.then(response => response.data)
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
