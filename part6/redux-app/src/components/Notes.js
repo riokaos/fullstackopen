e216771d9c2ea10c7ad13d8c4704d9395c51c4f8
Carl B <petter@axesnet.com>
2020-08-14 10:42:09 +0200

    part 6d in redux notes redux-app done

--- part6/redux-app/src/components/Notes.js
+++ part6/redux-app/src/components/Notes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { connect, useDispatch } from 'react-redux'
 import { toggleImportanceOf } from '../reducers/noteReducer'
 
 const Note = ({ note, handleClick }) => {
@@ -11,26 +11,38 @@ const Note = ({ note, handleClick }) => {
   )
 }
 
-const Notes = () => {
-  const dispatch = useDispatch()
-  const notes = useSelector(({ filter, notes }) => {
-    if(filter ==='ALL'){
-      return notes
-    }
-    return filter === 'IMPORTANT'
-      ? notes.filter(note => note.important)
-      : notes.filter(note => !note.important)
-  })
-  console.log("notes:",notes);
+const Notes = (props) => {
+  // const dispatch = useDispatch()
+  // const notes = useSelector(({ filter, notes }) => {
+  //   if(filter ==='ALL'){
+  //     return notes
+  //   }
+  //   return filter === 'IMPORTANT'
+  //     ? notes.filter(note => note.important)
+  //     : notes.filter(note => !note.important)
+  // })
+  // console.log("notes:",notes);
+
+// old way using connect
+// get list of Notes
+  // const notesToShow = () => {
+  //     if ( props.filter === 'ALL ') {
+  //       return props.notes
+  //     }
+  //
+  //     return props.filter  === 'IMPORTANT'
+  //       ? props.notes.filter(note => note.important)
+  //       : props.notes.filter(note => !note.important)
+  //   }
 
   return(
     <ul>
-      {notes.map(note =>
+      {props.notes.map(note =>
         <Note
           key={note.id}
           note={note}
           handleClick={() =>
-            dispatch(toggleImportanceOf(note.id))
+            props.toggleImportanceOf(note.id)
           }
         />
       )}
@@ -38,4 +50,28 @@ const Notes = () => {
   )
 }
 
-export default Notes
+const mapStateToProps = (state) => {
+  if ( state.filter === 'ALL' ) {
+    return {
+      notes: state.notes
+    }
+  }
+  
+  return {
+    notes: (state.filter  === 'IMPORTANT'
+      ? state.notes.filter(note => note.important)
+      : state.notes.filter(note => !note.important)
+    )
+  }
+}
+const mapDispatchToProps = {
+  toggleImportanceOf,
+}
+
+const ConnectedNotes = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notes)
+export default ConnectedNotes
+
+// export default Notes
--- part6/redux-app/src/reducers/noteReducer.js
+++ part6/redux-app/src/reducers/noteReducer.js
@@ -1,16 +1,4 @@
 import noteService from '../services/notes'
-const initialState = [
-  {
-    content: 'reducer defines how redux store works',
-    important: true,
-    id: 1,
-  },
-  {
-    content: 'state of store can contain any data',
-    important: false,
-    id: 2,
-  },
-]
 
 const noteReducer = (state = [], action) => {
   console.log('ACTION: ', action)
