ddc41034844670e2af1435bc0592a430bdd3dcf4
Carl B <petter@axesnet.com>
2020-09-24 11:54:00 +0200

    part 7c proyect

---
+++ part7/proy_7c/src/App.js
@@ -0,0 +1,45 @@
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
+import PromisePolyfill from 'promise-polyfill'
+
+if (!window.Promise) {
+  window.Promise = PromisePolyfill
+}
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  useEffect(() => {
+    axios.get(url).then(response => {
+      setNotes(response.data)
+    })
+  }, [url])
+  return notes
+}
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const [values, setValues] = useState([])
+  const notes = useNotes(BACKEND_URL)
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    setValues(values.concat(counter))
+  }
+
+    return (
+      <div className="container">
+        hello webpack {counter} clicks
+        <button onClick={handleClick}>
+          press
+        </button>
+        <button onClick={() => setCounter(counter - 1)}>
+          press downs
+        </button>
+        <div>{notes.length} notes on server {BACKEND_URL}</div>
+      </div>
+    )
+
+}
+
+export default App
